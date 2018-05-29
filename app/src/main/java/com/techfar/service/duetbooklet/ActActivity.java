4619ff954e0ef7b310bf78c5d766772445583aee
GMuktadir <gm.cse.duet@gmail.com>
2018-05-29 15:38:10 +0600

    Add pdf viewer dependencies in act activity

--- app/src/main/java/com/techfar/service/duetbooklet/ActActivity.java
+++ app/src/main/java/com/techfar/service/duetbooklet/ActActivity.java
@@ -3,11 +3,17 @@ package com.techfar.service.duetbooklet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class ActActivity extends AppCompatActivity {
+    PDFView act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act);
+        act=(PDFView) findViewById(R.id.pdfact);
+
+        act.fromAsset("duetact.pdf").load();
     }
 }
